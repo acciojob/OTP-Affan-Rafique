fb8b156a5ba7290d10dbc89f350c1ff53e387959
Affan-Rafique <affanrafique9@gmail.com>
2025-10-04 13:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+    codes[0].focus(); // Focus first input on page load
+
+    codes.forEach((code, idx) => {
+      code.addEventListener('input', (e) => {
+        const value = e.target.value;
+
+        // If user typed something, move to next
+        if (value && idx < codes.length - 1) {
+          codes[idx + 1].focus();
+        }
+      });
+
+      code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+          if (e.target.value === '' && idx > 0) {
+            // Move to previous field and clear it
+            codes[idx - 1].focus();
+            codes[idx - 1].value = '';
+          }
+        }
+      });
+    });
